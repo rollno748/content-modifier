0b2bc077ff6bd17e317c14ec245a6d954c022d16
rollno748 <rollno748@gmail.com>
2023-02-02 20:34:41 +0000

    Partially working UI

---
+++ src/main/java/com/fspace/config/ContentModifierPreProcessor.java
@@ -0,0 +1,61 @@
+package com.fspace.config;
+
+import org.apache.jmeter.processor.PreProcessor;
+import org.apache.jmeter.testelement.AbstractTestElement;
+import org.apache.jmeter.testelement.TestStateListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ContentModifierPreProcessor extends AbstractTestElement implements PreProcessor, TestStateListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContentModifierPreProcessor.class);
+
+    public static final String USE_STRING = "string";
+    public static final String USE_JSON = "json";
+    public static final String USE_REGEX = "regex";
+    public static final String USE_XPATH = "xpath";
+    public static final String SOURCE_CONTENT = "sourceContent";
+    public static final String QUERY_PATH = "queryPath";
+
+
+    public ContentModifierPreProcessor(){
+    }
+
+    @Override
+    public void process() {
+
+    }
+
+    @Override
+    public void testStarted() {
+
+    }
+    @Override
+    public void testEnded() {
+    }
+    @Override
+    public void testStarted(String host) {
+        testStarted();
+    }
+    @Override
+    public void testEnded(String host) {
+        testEnded();
+    }
+
+    public String getReplacerType() {
+        return getPropertyAsString("");
+    }
+
+    public String getSourceContent() {
+        return "Hello World";
+    }
+
+    public String getQueryPath() {
+        return ".+";
+    }
+}
+
+//    private static final String USE_STRING = "string";
+//    private static final String USE_JSON = "json";
+//    private static final String USE_REGEX = "regex";
+//    private static final String USE_XPATH = "xpath";
